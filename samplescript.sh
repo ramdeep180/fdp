87b23585a1aca26c7dc97ff09d7b13ce3574a3cf
root <root@ip-172-31-82-221.ec2.internal>
2020-05-20 06:13:48 +0000

    changed all echo values

--- samplescript.sh
+++ samplescript.sh
@@ -5,6 +5,6 @@ P=$(curl -sL http://169.254.169.254/latest/meta-data/profile)
 PIP=$(curl -sL http://169.254.169.254/latest/meta-data/public-ipv4)
 echo "My instance ID is $II"
 echo "My instance MAC address is $MAC"
-echo "$P"
-echo "$PIP"
+echo "My instance profile is $P"
+echo "my instance public ip is $PIP"
 
